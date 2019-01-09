4871c5134e84bc8ea8b5cb768660e700961bf0dd
Ville Seppanen <ville.seppanen@gofore.com>
2019-01-09 21:37:49 +0200

    Refactor training to be larva-centric

--- bot/main.py
+++ bot/main.py
@@ -26,6 +26,7 @@ class MyBot(sc2.BotAI):
 
     def on_start(self):
         self.last_cap_covered = 0
+        self.hq_loss_handled = False
         logger.info("Game started, gl hf!")
 
     def on_end(self, result):
@@ -36,6 +37,14 @@ class MyBot(sc2.BotAI):
         cap_usage = "{}/{}".format(self.supply_used, self.supply_cap)
         logger.log(level, "{:4.1f} {:7} {}".format(time_in_minutes, cap_usage, msg))
 
+    def should_train_overlord(self):
+        if self.units(OVERLORD).amount == 1:
+            cap_safety_buffer = 0
+        else:
+            cap_safety_buffer = 2 * len(self.townhalls)
+        should = self.supply_left <= cap_safety_buffer and self.supply_cap != self.last_cap_covered
+        return should
+
     async def on_step(self, iteration):
         larvae = self.units(LARVA)
         forces = self.units(ZERGLING) | self.units(ROACH) | self.units(HYDRALISK)
@@ -43,64 +52,59 @@ class MyBot(sc2.BotAI):
 
         # Kamikaze if HQ lost
         if not self.townhalls.exists:
-            self.log("HQ lost, kamikazeing!", logging.WARNING)
-            for unit in self.units(DRONE) | self.units(QUEEN) | forces:
-                actions.append(unit.attack(self.enemy_start_locations[0]))
-            await self.do_actions(actions)
+            if not self.hq_loss_handled:
+                self.hq_loss_handled = True
+                self.log("HQ lost, loss is probably imminent!", logging.WARNING)
+                for unit in self.units(DRONE) | self.units(QUEEN) | forces:
+                    actions.append(unit.attack(self.enemy_start_locations[0]))
+                await self.do_actions(actions)
             return
         else:
             hq = self.townhalls.first
 
-        if self.minerals > 400:
-            self.log("Too many minerals, building another hatchery")
-            await self.build(HATCHERY, near=hq)
-        if self.vespene > 1000 and iteration % 10 == 0:
-            self.log("Too much gas", logging.WARNING)
-        if self.supply_left == 0 and iteration % 50 == 0:
-            self.log("Not enough overlords!", logging.WARNING)
+        # Attack to enemy base
+        if self.units(ROACH).amount > 10 and iteration % 50 == 0:
+            if len(forces.idle) > 0:
+                self.log("Ordering {} forces to attack".format(len(forces.idle)), logging.DEBUG)
+                for unit in forces.idle:
+                    actions.append(unit.attack(self.select_target()))
 
-        # Basic needs
-        cap_buffer = 2 * len(self.townhalls)
-        if self.supply_left < cap_buffer and self.supply_cap != self.last_cap_covered:
-            if self.can_afford(OVERLORD) and larvae.exists:
-                self.log("Training overlord, cap buffer required " + str(cap_buffer), logging.DEBUG)
-                actions.append(larvae.random.train(OVERLORD))
-                self.last_cap_covered = self.supply_cap
-                await self.do_actions(actions)
-                return
-        else:
-            if hq.assigned_harvesters < hq.ideal_harvesters:
-                if self.can_afford(DRONE) and larvae.exists:
-                    larva = larvae.random
-                    self.log("Training drone, currently assigned {}/{}".format(hq.assigned_harvesters, hq.ideal_harvesters), logging.DEBUG)
-                    actions.append(larva.train(DRONE))
+        # Training units
+        if larvae.exists:
+            if self.should_train_overlord():
+                if self.can_afford(OVERLORD):
+                    self.log("Training overlord", logging.DEBUG)
+                    actions.append(larvae.random.train(OVERLORD))
+                    self.last_cap_covered = self.supply_cap
                     await self.do_actions(actions)
                     return
-
-
-        # Train roach
-        if self.units(ROACHWARREN).ready.exists:
-            if self.can_afford(ROACH) and larvae.exists:
-                self.log("Training roach", logging.DEBUG)
-                # log.info(self.state.game_loop)
-                actions.append(larvae.random.train(ROACH))
-                await self.do_actions(actions)
-                return
-
-        # AGGRO
-        if self.units(ROACH).amount > 10 and iteration % 50 == 0:
-            self.log("Ordering forces to attack", logging.DEBUG)
-            for unit in forces.idle:
-                actions.append(unit.attack(self.select_target()))
-
-        # More larvas
-        for queen in self.units(QUEEN).idle:
-            abilities = await self.get_available_abilities(queen)
-            if AbilityId.EFFECT_INJECTLARVA in abilities:
-                self.log("Queen creating larvae", logging.DEBUG)
-                actions.append(queen(EFFECT_INJECTLARVA, hq))
+            elif hq.assigned_harvesters < hq.ideal_harvesters:
+                if self.can_afford(DRONE):
+                    self.log("Training drone, currently assigned {}/{}".format(hq.assigned_harvesters, hq.ideal_harvesters), logging.DEBUG)
+                    actions.append(larvae.random.train(DRONE))
+                    await self.do_actions(actions)
+                    return
+            elif self.units(ROACHWARREN).ready.exists:
+                if self.can_afford(ROACH) and larvae.exists:
+                    actions.append(larvae.random.train(ROACH))
+                    self.log("Training roach", logging.DEBUG)
+                    await self.do_actions(actions)
+                    return
+            # TODO make as many queens
+            elif self.units(SPAWNINGPOOL).ready.exists:
+                if not self.units(QUEEN).exists and hq.is_ready and hq.noqueue:
+                    if self.can_afford(QUEEN):
+                        self.log("Training queen", logging.DEBUG)
+                        actions.append(hq.train(QUEEN))
+            elif self.units(ZERGLING).amount < 20 and self.minerals > 1000:
+                if larvae.exists and self.can_afford(ZERGLING):
+                    self.log("Training ling")
+                    actions.append(larvae.random.train(ZERGLING))
 
         # Build tree
+        if self.can_afford(HATCHERY):
+            self.log("Building hatchery")
+            await self.build(HATCHERY, near=hq)
         if not (self.units(SPAWNINGPOOL).exists or self.already_pending(SPAWNINGPOOL)):
             if self.can_afford(SPAWNINGPOOL):
                 self.log("Building spawning pool")
@@ -117,7 +121,12 @@ class MyBot(sc2.BotAI):
                     self.log("Building roach warren")
                     await self.build(ROACHWARREN, near=hq)
 
-
+        # Rare, low-priority actions
+        for queen in self.units(QUEEN).idle:
+            abilities = await self.get_available_abilities(queen)
+            if AbilityId.EFFECT_INJECTLARVA in abilities:
+                self.log("Queen creating larvae", logging.DEBUG)
+                actions.append(queen(EFFECT_INJECTLARVA, hq))
         for extractor in self.units(EXTRACTOR):
             if extractor.assigned_harvesters < extractor.ideal_harvesters:
                 worker = self.workers.closer_than(20, extractor)
@@ -125,15 +134,13 @@ class MyBot(sc2.BotAI):
                     self.log("Assigning drone to extractor", logging.DEBUG)
                     actions.append(worker.random.gather(extractor))
 
-        if self.units(SPAWNINGPOOL).ready.exists:
-            if not self.units(QUEEN).exists and hq.is_ready and hq.noqueue:
-                if self.can_afford(QUEEN):
-                    self.log("Training queen", logging.DEBUG)
-                    actions.append(hq.train(QUEEN))
+        # Warnings
+        if self.vespene > 600 and iteration % 20 == 0:
+            self.log("Too much gas", logging.WARNING)
+        if self.supply_left == 0 and iteration % 30 == 0:
+            self.log("Not enough overlords!", logging.WARNING)
+        if hq.assigned_harvesters > hq.ideal_harvesters and iteration % 20 == 0:
+            self.log("Overassigned drones, should expand!", logging.WARNING)
 
-        if self.units(ZERGLING).amount < 20 and self.minerals > 1000:
-            if larvae.exists and self.can_afford(ZERGLING):
-                self.log("Training ling")
-                actions.append(larvae.random.train(ZERGLING))
 
         await self.do_actions(actions)
