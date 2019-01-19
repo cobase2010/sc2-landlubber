37cb6fc0c8faeeb178bedaf2accb56529f87b68a
Ville Seppanen <ville.seppanen@gofore.com>
2019-01-19 19:45:51 +0200

    Hey let's fix again. TDTF

--- bot/economy.py
+++ bot/economy.py
@@ -103,7 +103,7 @@ async def produce_larvae(bot):
 def assign_idle_drones_to_minerals(bot):
     actions = []
     for drone in bot.units(DRONE).idle:
-        new_town = get_town_with_free_jobs(bot.townhalls, old_town)
+        new_town = get_town_with_free_jobs(bot.townhalls)
         if new_town:
             bot.log("Reassigning idle drone", logging.DEBUG)
             mineral = get_closest_mineral_for_hatchery(bot.state.mineral_field(), new_town)
