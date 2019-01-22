090b973e4174fb847cff201878a3f16212d0fc5d
Ville Seppanen <ville.seppanen@gofore.com>
2019-01-22 21:09:43 +0200

    Fix attack decision bug

--- bot/army.py
+++ bot/army.py
@@ -3,6 +3,7 @@ from sc2.constants import *
 
 ARMY_SIZE_BASE_LEVEL = 5
 ARMY_SIZE_TIME_MULTIPLIER = 3
+ARMY_SIZE_MAX = 180
 
 def get_army_strength(units):
     single_food = units(ZERGLING).ready.amount
@@ -23,7 +24,7 @@ def get_army_actions(iteration, forces, rally, enemy_structures, enemy_start_loc
     if iteration % 30 == 0:
         strength = get_army_strength(units)
         enough = (ARMY_SIZE_BASE_LEVEL + ((time / 60) * ARMY_SIZE_TIME_MULTIPLIER))
-        if strength >= enough or supply_used:
+        if strength >= enough or supply_used > ARMY_SIZE_MAX:
             for unit in forces:
                 actions.append(unit.attack(nearest_enemy_building(rally, enemy_structures, enemy_start_locations)))
     return actions
