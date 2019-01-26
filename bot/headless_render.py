127faaa5b981df4f82dd20bb6f5cfe203d37c429
Ville Seppänen <ville.seppanen@gofore.com>
2019-01-27 01:14:56 +0200

    Tweak headless render

--- bot/headless_render.py
+++ bot/headless_render.py
@@ -1,8 +1,41 @@
-import emoji
+# import emoji
+from sc2.constants import *
 
-DRAW_WIDTH = 200
+DRAW_WIDTH = 140
+
+class color:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD_RED = '\033[1;31m'
+    END = '\033[0m'
+
+def green(str):
+    return color.GREEN + str + color.END
+
+def red(str):
+    return color.BOLD_RED + str + color.END
+
+def yellow(str):
+    return color.YELLOW + str + color.END
+
+ICON_DOOR = "|"
+ICON_RALLY = "r"
+ICON_WAYPOINT = "v"
+ICON_ARMY_FRIENDLY = green(">")
+ICON_ARMY_ENEMY = red("<")
+ICON_ARMY_CONTESTED = yellow("X")
+
+# ICON_DOOR = ":construction:"
+# ICON_RALLY = ":anchor:"
+# ICON_WAYPOINT = ":round_pushpin:"
+# ICON_ARMY_FRIENDLY = ":octopus:"
+# ICON_ARMY_ENEMY = ":panda_face:"
+# ICON_ARMY_CONTESTED = ":fire:"
+# ICON_FIGHTING = ":collision:"
 
 def render_army(bot, forces_idle):
+    # try:
     row = [" "] * DRAW_WIDTH
     line_start = bot.start_location
     if bot.enemy_known_base_locations:
@@ -15,16 +48,37 @@ def render_army(bot, forces_idle):
     def px(point):
         distance = line_start.distance_to(point)
         pixel = int(distance / distance_per_pixel)
-        return pixel
+        if pixel > DRAW_WIDTH - 1:
+            return DRAW_WIDTH - 1
+        else:
+            return pixel
 
-    row[px(bot.hq_front_door)] = ":house:"
-    row[px(bot.army_spawn_rally_point)] = "s"
-    row[px(bot.army_attack_point)] = "X"
+    # row[0] = ":house:"
+    # row[-1] = ":post_office:"
 
-    if forces_idle:
-        row[px(forces_idle.center)] = ":pig:"
+    forces = bot.units(ZERGLING) | bot.units(ROACH) | bot.units(HYDRALISK) | bot.units(MUTALISK)
+    for unit in forces:
+        pos = px(unit.position)
+        row[pos] = ICON_ARMY_FRIENDLY
+
+    for enemy in bot.known_enemy_units:
+        pos = px(enemy.position)
+        if row[pos] == ICON_ARMY_FRIENDLY:
+            row[pos] = ICON_ARMY_CONTESTED
+        elif row[pos] == ICON_ARMY_CONTESTED:
+            pass
+        else:
+            row[pos] = ICON_ARMY_ENEMY
+
+    row[px(bot.hq_front_door)] = ICON_DOOR
+    row[px(bot.army_spawn_rally_point)] = ICON_RALLY
+    row[px(bot.army_attack_point)] = ICON_WAYPOINT
 
     row_string = ""
     for c in row:
         row_string += c
-    print(emoji.emojize(row_string))
+
+    print(row_string)
+    # print(emoji.emojize(row_string))
+    # except Exception as e:
+    #     print(e)
