978b3d5aaa28b1e6aa283f054508b7a74421476e
Ville Seppanen <ville.seppanen@gofore.com>
2019-01-29 19:56:02 +0200

    Expand more easily

--- bot/economy.py
+++ bot/economy.py
@@ -3,7 +3,7 @@ from sc2.constants import *
 import bot.util as util
 
 HATCHERY_COST = 300
-HATCHERY_COST_BUFFER_INCREMENT = 50
+HATCHERY_COST_BUFFER_INCREMENT = 40
 EXPANSION_DRONE_THRESHOLD = 0.80
 MAX_NUMBER_OF_DRONES = 70
 DRONE_TRAINING_PROBABILITY_AT_EXPANSIONS = 90
@@ -24,7 +24,7 @@ def should_build_hatchery(bot):
     if not bot.townhalls.not_ready and not bot.units.find_by_tag(bot.active_expansion_builder):
         if len(bot.units(QUEEN)) >= len(bot.townhalls):
             if drone_rate_for_towns(bot.townhalls) >= EXPANSION_DRONE_THRESHOLD and len(bot.expansions_sorted) > 0:
-                if bot.minerals >= HATCHERY_COST + (HATCHERY_COST_BUFFER_INCREMENT * len(bot.townhalls)):
+                if bot.minerals >= HATCHERY_COST + (HATCHERY_COST_BUFFER_INCREMENT * (len(bot.townhalls) - 1)):
                     return True
     return False
 
